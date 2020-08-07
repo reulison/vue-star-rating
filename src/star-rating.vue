900775c261350d52bbc8b0b80833360c104c11a3
Réulison Silva <eu@reulison.com.br>
2020-08-07 16:52:51 -0300

    Update star-rating.vue

--- src/star-rating.vue
+++ src/star-rating.vue
@@ -1,8 +1,12 @@
 <template>
     <div :class="['vue-star-rating', {'vue-star-rating-rtl':rtl}, {'vue-star-rating-inline': inline}]">
         <div @mouseleave="resetRating" class="vue-star-rating">
-            <span @click.once="incrementCounter" v-for="n in maxRating" :key="n" :class="[{'vue-star-rating-pointer': !readOnly }, 'vue-star-rating-star']" :style="{'margin-right': margin + 'px'}">
-              <star :fill="fillLevel[n-1]" :size="starSize" :points="starPoints" :star-id="n" :step="step" :active-color="activeColor" :inactive-color="inactiveColor" :border-color="borderColor" :border-width="borderWidth" :rounded-corners="roundedCorners" @star-selected="setRating($event, true)" @star-mouse-move="setRating" :rtl="rtl" :glow="glow" :glow-color="glowColor"></star>
+            <span v-for="n in maxRating" :key="n" :class="[{'vue-star-rating-pointer': !readOnly }, 'vue-star-rating-star']" :style="{'margin-right': margin + 'px'}">
+              <star :fill="fillLevel[n-1]" :size="starSize" :points="starPoints" :star-id="n" :step="step" :active-color="activeColor" :inactive-color="inactiveColor" 
+              :border-color="borderColor" :border-width="borderWidth" :rounded-corners="roundedCorners"
+              @click.once="incrementCounter ($event, true)"
+              @star-selected="setRating($event, true)" 
+              @star-mouse-move="setRating" :rtl="rtl" :glow="glow" :glow-color="glowColor"></star>
             </span>
             <span v-if="showRating" :class="['vue-star-rating-rating-text', textClass]"> {{formattedRating}}</span>
         </div>
